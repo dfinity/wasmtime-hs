4e2a2a5f9a7b2a65bf6f84ba58628ec93c1c806f
Bas van Dijk <bas@dfinity.org>
2023-05-28 19:23:48 +0000

    flake.nix: support x86_64-linux

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "aarch64-darwin" ] (system:
+    flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-linux" ] (system:
       let
         pkgs = import nixpkgs {
           inherit system;
@@ -26,14 +26,19 @@
 
         wasmtime = pkgs.wasmtime;
 
+        haskellPackages = pkgs.haskellPackages;
+
         # TODO: filter source to only include files added to git and strip flake.* files.
         src = ./.;
-        wasmtime-hs = pkgs.haskellPackages.callCabal2nix "wasmtime" src {};
+        wasmtime-hs = haskellPackages.callCabal2nix "wasmtime" src {};
       in
       {
         packages.default = wasmtime-hs;
         packages.wasmtime = pkgs.wasmtime.dev;
-        devShells.default = wasmtime-hs.envFunc {};
+        devShells.default = haskellPackages.shellFor {
+          packages = _: [ wasmtime-hs ];
+          nativeBuildInputs = [ pkgs.cabal-install ];
+        };
       }
     );
 }
