1aaa76be79dd4059098eddcab561fc5f6d5a7979
Michael Weigelt <michael.weigelt@dfinity.org>
2023-06-09 16:24:50 +0200

    redid assertion

--- test/memory.hs
+++ test/memory.hs
@@ -1,18 +1,17 @@
+{-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 -- | Haskell translation of: https://docs.wasmtime.dev/examples-c-memory.html
 module Main (main) where
 
 import Control.Exception (Exception, throwIO)
-import Control.Exception.Base (assert)
+import Control.Monad (unless)
 import Control.Monad.Primitive (MonadPrim, RealWorld)
 import qualified Data.ByteString as B
 import Data.Int (Int32)
-import qualified Data.List.NonEmpty as BI
-import Data.Primitive.Ptr (advancePtr)
 import Data.Word (Word8)
-import Foreign (poke)
 import Paths_wasmtime (getDataFileName)
 import System.IO (BufferMode (NoBuffering), hSetBuffering, stdout)
 import Wasmtime
@@ -45,13 +44,13 @@ main = do
 
   putStrLn "Checking memory..."
   size_pages <- getMemorySizePages ctx memory
-  let _ = assert (size_pages == 2) ()
+  assertBool "Failed size_pages 1" $ size_pages == 2
   size_bytes <- getMemorySizeBytes ctx memory
-  let _ = assert (size_bytes == 0x20000) ()
+  assertBool "Failed size_bytes 1" $ size_bytes == 0x20000
   mem_bs <- readMemory ctx memory
-  let _ = assert (B.head mem_bs == 0) ()
-  let _ = assert (B.index mem_bs 0x1000 == 1) ()
-  let _ = assert (B.index mem_bs 0x1003 == 4) ()
+  assertBool "Failed memory 0" $ B.head mem_bs == 0
+  assertBool "Failed memory 0x1000" $ B.index mem_bs 0x1000 == 1
+  assertBool "Failed memory 0x1003" $ B.index mem_bs 0x1003 == 4
   -- check function calls
   Right 2 <- callFunc ctx sizeFun
   Right 0 <- callFunc ctx loadFun 0
@@ -59,16 +58,16 @@ main = do
   Right 4 <- callFunc ctx loadFun 0x1003
   Right 0 <- callFunc ctx loadFun 0x1ffff
   Left trap_res <- callFunc ctx loadFun 0x20000
-  let _ = assert (trapCode trap_res == Just TRAP_CODE_MEMORY_OUT_OF_BOUNDS) ()
+  assertBool "Failed trap code 1" $ trapCode trap_res == Just TRAP_CODE_MEMORY_OUT_OF_BOUNDS
 
   putStrLn "Mutating memory..."
-  writeByte ctx memory 0x1003 5
+  Right () <- writeByte ctx memory 0x1003 5
   Right () <- callFunc ctx storeFun 0x1002 6
   Left trap_res <- callFunc ctx storeFun 0x20000 0
-  let _ = assert (trapCode trap_res == Just TRAP_CODE_MEMORY_OUT_OF_BOUNDS) ()
+  assertBool "Failed trap code 2" $ trapCode trap_res == Just TRAP_CODE_MEMORY_OUT_OF_BOUNDS
   mem_bs <- readMemory ctx memory
-  let _ = assert (B.index mem_bs 0x1002 == 6) ()
-  let _ = assert (B.index mem_bs 0x1003 == 5) ()
+  assertBool "Failed memory 0x1002" $ B.index mem_bs 0x1002 == 6
+  assertBool "Failed memory 0x1003 2" $ B.index mem_bs 0x1003 == 5
   Right 6 <- callFunc ctx loadFun 0x1002
   Right 5 <- callFunc ctx loadFun 0x1003
 
@@ -79,10 +78,10 @@ main = do
   Right 0 <- callFunc ctx loadFun 0x20000
   Right () <- callFunc ctx storeFun 0x20000 0
   Left trap_res <- callFunc ctx loadFun 0x30000
-  let _ = assert (trapCode trap_res == Just TRAP_CODE_MEMORY_OUT_OF_BOUNDS) ()
+  assertBool "Failed trap code 3" $ trapCode trap_res == Just TRAP_CODE_MEMORY_OUT_OF_BOUNDS
   Left trap_res <- callFunc ctx storeFun 0x30000 0
-  let _ = assert (trapCode trap_res == Just TRAP_CODE_MEMORY_OUT_OF_BOUNDS) ()
-  Left trap_res <- growMemory ctx memory 1
+  assertBool "Failed trap code 4" $ trapCode trap_res == Just TRAP_CODE_MEMORY_OUT_OF_BOUNDS
+  Left _ <- growMemory ctx memory 1
   Right 3 <- growMemory ctx memory 0
 
   putStrLn "Creating stand-alone memory..."
@@ -104,13 +103,8 @@ wasmFromPath path = do
   bytes <- getDataFileName path >>= B.readFile
   handleWasmtimeError $ wat2wasm bytes
 
--- TODO: this is unsafe and does not check if the offset is within the linear memory
--- replace with writeMemory!
-unsafeWriteByte :: Context s -> Memory s -> Int -> Word8 -> IO ()
-unsafeWriteByte ctx mem offset byte =
-  unsafeWithMemory ctx mem $ \start_ptr _maxlen -> do
-    let pos_ptr = advancePtr start_ptr offset
-    poke pos_ptr byte
-
 writeByte :: MonadPrim s m => Context s -> Memory s -> Int -> Word8 -> m (Either MemoryAccessError ())
 writeByte ctx mem offset byte = writeMemory ctx mem offset $ B.singleton byte
+
+assertBool :: String -> Bool -> IO ()
+assertBool msg bool = unless bool $ putStrLn msg
