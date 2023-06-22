15b34f838f8a72931894ee0dd502b3740b5722df
Michael Weigelt <michael.weigelt@dfinity.com>
2023-06-22 08:38:45 +0000

    wip

--- test/memtest.hs
+++ test/memtest.hs
@@ -1,19 +1,72 @@
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main (main) where
 
-import Control.Exception (Exception, throwIO, try)
-import Control.Monad.Primitive (RealWorld)
+import Control.Exception (Exception, throwIO)
+import Control.Monad.Primitive (MonadPrim, RealWorld)
 import qualified Data.ByteString as B
 import Data.Foldable (for_)
 import Data.Int (Int32)
+import Data.Word (Word8)
 import Paths_wasmtime (getDataFileName)
 import System.IO (BufferMode (NoBuffering), hSetBuffering, stdout)
 import Test.Tasty.HUnit ((@?=))
 import Wasmtime
+import Wasmtime (callFunc)
 
 main :: IO ()
 main = do
-  putStrLn "ok"
+  hSetBuffering stdout NoBuffering
 
+  putStrLn "Initializing..."
+  engine <- newEngine
+  store <- newStore engine
+  ctx <- storeContext store
+  wasm <- wasmFromPath "test/c.wat"
+  --   wasm <- wasmFromPath' "test/c.wasm"
 
+  putStrLn "Compiling module..."
+  myModule <- handleWasmtimeError $ newModule engine wasm
 
+  for_ ([1 ..] :: [Int32]) $ \i -> do
+    print i
+    f <- newFunc ctx msg_reply
 
+    putStrLn "Instantiating module..."
+    inst <- newInstance ctx myModule [toExtern f]
+    inst <- case inst of
+      Left trap -> do
+        print ("trap", trapCode trap)
+        error "nok"
+      Right inst -> do
+        putStrLn "ok got inst"
+        pure inst
+
+    putStrLn "Extracting exports..."
+    --   Just memory <- getExportedMemory ctx inst "memory"
+    Just (init :: TypedFunc RealWorld (IO (Either Trap ()))) <- getExportedTypedFunc ctx inst "canister_init"
+    Just (readf :: TypedFunc RealWorld (IO (Either Trap ()))) <- getExportedTypedFunc ctx inst "canister_query get"
+
+    res <- callFunc ctx init
+    print ("res", res)
+    res <- callFunc ctx readf
+    print ("res", res)
+
+  putStrLn "end"
+
+msg_reply :: IO (Either Trap ())
+msg_reply = return $ Right ()
+
+handleWasmtimeError :: Either WasmtimeError a -> IO a
+handleWasmtimeError = either throwIO pure
+
+wasmFromPath :: FilePath -> IO Wasm
+wasmFromPath path = do
+  bytes <- getDataFileName path >>= B.readFile
+  handleWasmtimeError $ wat2wasm bytes
+
+wasmFromPath' :: FilePath -> IO Wasm
+wasmFromPath' path = do
+  bytes <- getDataFileName path >>= B.readFile
+  pure $ unsafeWasmFromBytes bytes
