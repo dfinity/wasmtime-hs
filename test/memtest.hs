21fdf349f878a6b316b698734bd0f91604e1c63f
Michael Weigelt <michael.weigelt@dfinity.com>
2023-06-23 13:29:44 +0000

    printing finalizers

--- test/memtest.hs
+++ test/memtest.hs
@@ -12,8 +12,11 @@ import Data.Word (Word8)
 import Paths_wasmtime (getDataFileName)
 import System.IO (BufferMode (NoBuffering), hSetBuffering, stdout)
 import Test.Tasty.HUnit ((@?=))
+import System.Mem (performGC)
 import Wasmtime
 import Wasmtime (callFunc)
+
+-- TODO: cmd line args for num iterations
 
 main :: IO ()
 main = do
@@ -21,42 +24,50 @@ main = do
 
   putStrLn "Initializing..."
   engine <- newEngine
-  store <- newStore engine
-  ctx <- storeContext store
-  wasm <- wasmFromPath "test/memtest.wat"
+  for_ ([1 ..] :: [Int32]) $ \j -> do
+    print ("j", j)
+    store <- newStore engine
+    ctx <- storeContext store
+    wasm <- wasmFromPath "test/memtest.wat"
 
-  putStrLn "Compiling module..."
-  myModule <- handleWasmtimeError $ newModule engine wasm
+    -- putStrLn "Compiling module..."
+    myModule <- handleWasmtimeError $ newModule engine wasm
 
-  for_ ([1 .. ] :: [Int32]) $ \i -> do
-    print i
-    fIo <- newFunc ctx io_imp
-    fPure <- newFunc ctx pure_imp
+    for_ ([1 .. 10] :: [Int32]) $ \i -> do
+      -- print ("i", i)
+      fIo <- newFunc ctx io_imp
+      fPure <- newFunc ctx pure_imp
 
-    putStrLn "Instantiating module..."
-    inst <- newInstance ctx myModule [toExtern fIo, toExtern fPure]
-    inst <- case inst of
-      Left trap -> do
-        print ("trap", trapCode trap)
-        error "nok"
-      Right inst -> do
-        putStrLn "ok got inst"
-        pure inst
+      -- putStrLn "Instantiating module..."
+      inst <- newInstance ctx myModule [toExtern fIo, toExtern fPure]
+      inst <- case inst of
+        Left trap -> do
+          print ("trap", trapCode trap)
+          error "nok"
+        Right inst -> do
+          -- putStrLn "ok got inst"
+          pure inst
 
-    putStrLn "Extracting exports..."
-    Just memory <- getExportedMemory ctx inst "memory"
-    Just (size :: TypedFunc RealWorld (IO (Either Trap Int32))) <- getExportedTypedFunc ctx inst "size"
-    Just (load :: TypedFunc RealWorld (Int32 -> IO (Either Trap Int32))) <- getExportedTypedFunc ctx inst "load"
-    Just (store :: TypedFunc RealWorld (Int32 -> Int32 -> IO (Either Trap ()))) <- getExportedTypedFunc ctx inst "store"
-    Just (callImportedPure :: TypedFunc RealWorld (Int32 -> IO (Either Trap Int32))) <- getExportedTypedFunc ctx inst "call_imported_pure"
-    Just (callImportedIo :: TypedFunc RealWorld ( IO (Either Trap ()))) <- getExportedTypedFunc ctx inst "call_imported_io"
+      -- putStrLn "Extracting exports..."
+      Just memory <- getExportedMemory ctx inst "memory"
+      Just (size :: TypedFunc RealWorld (IO (Either Trap Int32))) <- getExportedTypedFunc ctx inst "size"
+      Just (load :: TypedFunc RealWorld (Int32 -> IO (Either Trap Int32))) <- getExportedTypedFunc ctx inst "load"
+      Just (store :: TypedFunc RealWorld (Int32 -> Int32 -> IO (Either Trap ()))) <- getExportedTypedFunc ctx inst "store"
+      Just (callImportedPure :: TypedFunc RealWorld (Int32 -> IO (Either Trap Int32))) <- getExportedTypedFunc ctx inst "call_imported_pure"
+      Just (callImportedIo :: TypedFunc RealWorld (IO (Either Trap ()))) <- getExportedTypedFunc ctx inst "call_imported_io"
 
-    Right res <- callFunc ctx size
-    print ("res", res)
-    Right res <- callFunc ctx load 3
-    print ("res", res)
-
-
+      Right res <- callFunc ctx size
+      -- print ("size", res)
+      Right () <- callFunc ctx store i i
+      Right res <- callFunc ctx load i
+      -- print ("load", res)
+      Right res <- callFunc ctx callImportedPure i
+      -- print ("pure", res)
+      -- Right () <- callFunc ctx callImportedIo
+      -- putStrLn "--------------"
+      pure ()
+    performGC
+    putStrLn "=================="
   -- x <- readLn
   -- putStrLn x
   putStrLn "end"
