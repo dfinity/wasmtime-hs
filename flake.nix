569610df975cf37773e658ad016efceea3e3ce2b
ncaq <ncaq@ncaq.net>
2025-02-11 20:41:01 +0900

    fix!: support new wasmtime(v29) (#48)
    Is it wasmtime-hs can support wasmtime version 29.0.1.
    
    This pull request includes several changes to the `flake.nix`
    configuration and updates to the Wasmtime Haskell bindings. The most
    important changes include the removal of the Wasmtime input from the Nix
    flake, updates to the Wasmtime configuration functions, and the addition
    of a buffering specification in a test file.
    
    ### Nix Configuration Updates:
    * Removed the Wasmtime input from the `flake.nix` file and added an
    overlay to build Wasmtime with C API support.
    [[1]](diffhunk://#diff-206b9ce276ab5971a2489d75eb1b12999d4bf3843b7988cbe8d687cfde61dea0L7-R30)
    [[2]](diffhunk://#diff-206b9ce276ab5971a2489d75eb1b12999d4bf3843b7988cbe8d687cfde61dea0L25-R50)
    
    ### Wasmtime Haskell Bindings Updates:
    * Replaced removed Wasmtime configuration functions with new ones in
    `lib/Bindings/Wasmtime/Config.hsc` and `lib/Wasmtime.hs`.
    [[1]](diffhunk://#diff-e98fea86c4690e7b94832c5dfe0184f51fb15dfc4d38eb1c6f5c340ffd083c79L53-R55)
    [[2]](diffhunk://#diff-d49ac0017ec35aa1a96fc2a1f36b5cef18166aa8e7ceaa7b1239df2adc7fe0a4L48-R50)
    [[3]](diffhunk://#diff-d49ac0017ec35aa1a96fc2a1f36b5cef18166aa8e7ceaa7b1239df2adc7fe0a4L472-R481)
    
    ### Test File Update:
    * Added a buffering specification in `test/memory.hs` to ensure the test
    runs correctly in the `nix build`.

--- flake.nix
+++ flake.nix
@@ -4,16 +4,30 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    wasmtime = {
-      url = "github:bytecodealliance/wasmtime/v21.0.0";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, wasmtime }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-linux" ] (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        overlays = [
+          (self: super: {
+            # To use c-api, build by cmake is also required.
+            # [wasmtime/crates/c-api at main · bytecodealliance/wasmtime](https://github.com/bytecodealliance/wasmtime/tree/b981b08c3c1c13276df9b3c0dc2cdae1de750572/crates/c-api)
+            wasmtime = super.wasmtime.overrideAttrs (oldAttrs: {
+              postInstall = ''
+                ${oldAttrs.postInstall or ""}
+                cmake -S crates/c-api -B target/c-api --install-prefix "$(pwd)/artifacts"
+                cmake --build target/c-api
+                cmake --install target/c-api
+                install -m0644 $(pwd)/artifacts/include/wasmtime/conf.h $dev/include/wasmtime
+              '';
+            });
+          })
+        ];
+
+        wasmtime = pkgs.wasmtime;
 
         haskellPackages = pkgs.haskellPackages;
 
@@ -22,30 +36,32 @@
         wasmtime-hs = haskellPackages.callCabal2nix "wasmtime-hs" src { };
 
         # Builds one of the C examples from $wasmtime/examples.
-        c_example = name: pkgs.runCommandCC name
-          {
-            inherit name wasmtime;
-            buildInputs = [ pkgs.wasmtime.dev ];
-          } ''
-          cc $wasmtime/examples/$name.c -lwasmtime -o $out
-        '';
+        c_example = name:
+          pkgs.runCommandCC name
+            {
+              inherit name wasmtime;
+              buildInputs = [ wasmtime.dev ];
+            } ''
+            cc $wasmtime/examples/$name.c -lwasmtime -o $out
+          '';
 
         # Runs one of the C $wasmtime/examples.
-        check_c_example = name: pkgs.runCommand (name + "-check")
-          {
-            inherit wasmtime;
-            example = c_example name;
-          } ''
-          cd $wasmtime
-          $example | tee $out
-        '';
+        check_c_example = name:
+          pkgs.runCommand (name + "-check")
+            {
+              inherit wasmtime;
+              example = c_example name;
+            } ''
+            cd $wasmtime
+            $example | tee $out
+          '';
       in
       {
         # By default we build the wasmtime-hs package.
         packages.default = wasmtime-hs;
 
         # We also export libwasmtime so we can quickly inspect it from the CLI.
-        packages.wasmtime = pkgs.wasmtime.dev;
+        packages.wasmtime = wasmtime.dev;
 
         # We build and run each C example from wasmtime
         # so we can compare them to the Haskell equivalents. Run them using:
@@ -62,13 +78,10 @@
         # box.
         devShells.default = haskellPackages.shellFor {
           packages = _: [ wasmtime-hs ];
-          nativeBuildInputs = [
-            pkgs.cabal-install
-            haskellPackages.haskell-language-server
-          ];
+          nativeBuildInputs =
+            [ pkgs.cabal-install haskellPackages.haskell-language-server ];
         };
         # so that we can format .nix code using: nix fmt
         formatter = pkgs.nixpkgs-fmt;
-      }
-    );
+      });
 }
