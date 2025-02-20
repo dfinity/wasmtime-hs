3c4cd26850598cca6c5c5fac8c33978dfd7fe4c6
kirisaki <endo.ryusuke@plus-zero.co.jp>
2025-02-20 18:53:13 +0900

    fix: give Caller the type variable s

--- test/caller.hs
+++ test/caller.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
 -- | Haskell translation of: https://docs.wasmtime.dev/examples-c-hello-world.html
@@ -11,10 +12,13 @@ import Paths_wasmtime_hs (getDataFileName)
 import Data.Int
 import Test.Tasty.HUnit ((@?=))
 import Wasmtime
+import Control.Monad.Primitive
 
 main :: IO ()
 main = do
   engine <- newEngine
+  
+  store <- newStore engine >>= handleException
 
   linker <- newLinker engine 
   linkerDefineFuncWithCaller linker "" "someFunc" someFunc >>= handleException
@@ -26,8 +30,6 @@ main = do
 
   myModule <- handleException $ newModule engine wasm
 
-  store <- newStore engine >>= handleException
-
   inst <- linkerInstantiate linker store myModule >>= handleException
 
   -- run x = someFunc (x + 1)
@@ -37,11 +39,12 @@ main = do
   result <- run 2 >>= handleException
   result @?= 6
 
-someFunc :: Caller -> Int32 -> IO (Either Trap Int32)
-someFunc caller x = Right <$> do
+someFunc :: s ~ PrimState IO => Caller s -> Int32 -> IO (Either Trap Int32)
+someFunc caller x =  do
   putStrLn $ "> someFunc got: " <> show x
   Just (double :: Int32 -> IO (Either WasmException Int32)) <- getExportedFunctionFromCaller caller "double"
-  double x >>= handleException
+  result <- double x >>= handleException
+  pure $ Right result
 
 handleException :: Exception e => Either e r -> IO r
 handleException = either throwIO pure
